d1cffed308cd7af917d9e700554d639605d49a28
fahimprito <ahmmedahmmed75@gmail.com>
2025-08-25 00:36:54 +0600

    cors fix

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,16 @@ import cors from "cors";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
 import cookieParser from "cookie-parser";
+import { envVars } from "./app/config/env";
 
 const app = express()
 
 app.use(express.json())
-app.use(cors())
 app.use(cookieParser())
+app.use(cors({
+    origin: envVars.FRONTEND_URL,
+    credentials: true
+}))
 
 app.use("/api", router)
 
--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -13,10 +13,11 @@ interface EnvConfig {
     JWT_REFRESH_EXPIRES: string
     SUPER_ADMIN_EMAIL: string
     SUPER_ADMIN_PASSWORD: string
+    FRONTEND_URL: string
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_SECRET", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRES", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASSWORD"];
+    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_SECRET", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRES", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASSWORD", "FRONTEND_URL"];
 
     requiredEnvVariables.forEach(key => {
         if (!process.env[key]) {
@@ -34,7 +35,8 @@ const loadEnvVariables = (): EnvConfig => {
         JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
         JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
         SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
-        SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string
+        SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
+        FRONTEND_URL: process.env.FRONTEND_URL as string
     }
 }
 
